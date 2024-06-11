47cf6c67000b6724e79d968803f855a7d9f49254
Evan <evan.j.ducas@gmail.com>
2024-06-11 18:26:30 -0400

    Added The Shop Menu
    The shop menu has full functionality. I also added the text on the left side that states how much of each currency you have, NO FUNCTIONALITY. Next I made a new script that we are going to use to put all the prefab references. I did this because I doing it all in the placing file is bad practice, and would make the code harder to read in my opinion. I also implemented text mesh pro if who ever is reading this, is wondering why there are emojis on the changed file Xd.

--- SpaceshipRougelike/Assets/PlacingScript.cs
+++ SpaceshipRougelike/Assets/PlacingScript.cs
@@ -4,41 +4,49 @@ using UnityEngine;
 
 public class PlacingScript : MonoBehaviour
 {
-    public GameObject TestObject;
-
     public float gridSize = 1.0f;          // Define the size of each grid cell
     public GameObject prefab;              // Reference to the prefab to be instantiated
     private HashSet<Vector2> occupiedCells; // Set to track occupied grid cells
-
+    
     private void Start()
     {
         occupiedCells = new HashSet<Vector2>();
+        PlaceInitialBuilding();
     }
 
     private void Update()
     {
-        // Check if the left mouse button is pressed
-        if (Input.GetMouseButtonDown(0))
-        {
-            // Get the mouse position in world space
-            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        HandleMouseInput();
+    }
 
-            // Snap the mouse position to the nearest grid point
+    // Place the initial building in the middle of the grid
+    private void PlaceInitialBuilding()
+    {
+        Vector2 centerPosition = SnapToGrid(Vector2.zero); // Center of the screen
+        GameObject initialBlock = Instantiate(prefab, centerPosition, Quaternion.identity);
+        occupiedCells.Add(centerPosition);
+        prefab = null;
+    }
+
+    // Handle mouse input for placing blocks
+    private void HandleMouseInput()
+    {
+        if (Input.GetMouseButtonDown(0) && prefab!=null)
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 gridPosition = SnapToGrid(mousePosition);
 
-            // Check if the grid position is adjacent to any existing blocks
-            if (IsPositionAdjacent(gridPosition) || occupiedCells.Count == 0)
+            // Check if the grid position is already occupied
+            if (!occupiedCells.Contains(gridPosition) && IsPositionAdjacent(gridPosition))
             {
-                // Instantiate the prefab at the snapped grid position
-                Instantiate(prefab, gridPosition, Quaternion.identity);
-
-                // Add the new block's position to the set of occupied cells
+                GameObject newBlock = Instantiate(prefab, gridPosition, Quaternion.identity);
                 occupiedCells.Add(gridPosition);
+
             }
         }
     }
 
-    // This method snaps a given position to the nearest grid point
+    // Snap a given position to the nearest grid point
     private Vector2 SnapToGrid(Vector2 originalPosition)
     {
         float x = Mathf.Round(originalPosition.x / gridSize) * gridSize;
@@ -46,7 +54,7 @@ public class PlacingScript : MonoBehaviour
         return new Vector2(x, y);
     }
 
-    // This method checks if the given position is adjacent to any existing block
+    // Check if the given position is adjacent to any existing block
     private bool IsPositionAdjacent(Vector2 position)
     {
         Vector2[] adjacentOffsets = new Vector2[]
@@ -68,4 +76,9 @@ public class PlacingScript : MonoBehaviour
         return false;
     }
 
+    public void SetPrefab(GameObject PrefabSetter)
+    {   
+        prefab = PrefabSetter;
+        Debug.Log("I GOT HERE :)");
+    }
 }
---
+++ SpaceshipRougelike/Assets/ShopManagerScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopManagerScript : MonoBehaviour
+{
+    public PlacingScript placingScript;
+    public GameObject Hallway; 
+    public void setHallway()
+    {
+    placingScript.SetPrefab(Hallway);
+        }
+}
