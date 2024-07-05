bcfab4d999900f9a2f6db0486deae3e33c29b0f0
Evan <evan.j.ducas@gmail.com>
2024-07-05 10:40:23 -0400

    Started work on the hull system :alien:
    read title

---
+++ SpaceshipRougelike/Assets/HullStrengthScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class HullStrengthScript : MonoBehaviour
+{
+    public int HullStrength = 0;
+    public int HowMuchHullStrengthToAdd = 0;
+    public TMP_Text HullText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HullText.text = "Current Hull Strength: " + HullStrength.ToString();
+        
+    }
+    public void ChangeMuchHull(int amount)
+    {
+        HowMuchHullStrengthToAdd = amount;
+    }
+    public int GetMuchHullStrength()
+    {
+        return HowMuchHullStrengthToAdd;
+    }
+    public void SetHowMuchHullToHullStrenght()
+    {
+        HullStrength = HowMuchHullStrengthToAdd;
+    }
+
+}
--- SpaceshipRougelike/Assets/Scripts/ShopManagerScript.cs
+++ SpaceshipRougelike/Assets/Scripts/ShopManagerScript.cs
@@ -15,32 +15,38 @@ public class ShopManagerScript : MonoBehaviour
     public GameObject basicGun;
     public bool needsArmory = true;
     public BuildingSystemAmtHandler BsAh;
+    public HullStrengthScript HSS;
     public void setHallway()
     {
-    placingScript.SetPrefab(Hallway, 0, 5,0,0,7, false, false, false);
-
-
+        placingScript.SetPrefab(Hallway, 0, 5, 0, 0, 7, false, false, false);
+        HSS.ChangeMuchHull(3);
     }
+
+    
     public void setThrustor()
     {
         placingScript.SetPrefab(Thrustor, 0, 2, 3, 0, 5, true, false, false);
         BsAh.setCurrAdd(2);
+        HSS.ChangeMuchHull(1);
     }
     public void setArmory()
     {
         placingScript.SetPrefab(Armory, 0, 3, 0, 1, 8, false, true, false);
         BsAh.setCurrAdd(0);
-    }
+        HSS.ChangeMuchHull(3);
+            }
     public void setSheildGenerator()
     {
         placingScript.SetPrefab(SheildGeneratpr, 2, 2, 1, 5, 10, false, false, false);
         BsAh.setCurrAdd(1);
-    }
+        HSS.ChangeMuchHull(2);
+            }
     public void setBasicGun()
     {
         placingScript.SetPrefab(basicGun, 3, 3, 1, 1, 3, true, false, true);
         BsAh.setCurrAdd(3);
-    }
+        HSS.ChangeMuchHull(1);
+            }
     public bool getArmory()
     {
         return needsArmory;
